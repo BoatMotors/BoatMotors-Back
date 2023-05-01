a0932c993a8570ed2930c8ece3975e321d8ab76a
Ibrohim1702 <88259165+Ibrohim1702@users.noreply.github.com>
2023-05-01 17:50:52 +0500

    some changes
    Email

--- src/settings.py
+++ src/settings.py
@@ -132,8 +132,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "ibrahimkabilov@gmail.com"
-EMAIL_HOST_PASSWORD = 'vrxpcqrdinxxijdz'
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
